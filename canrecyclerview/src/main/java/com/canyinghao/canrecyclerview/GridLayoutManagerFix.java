022e114b764cd780874ff3363271821e0b2082a8
canyinghao <canyinghao@hotmail.com>
2017-02-21 10:24:04 +0800

    1.1.7

---
+++ canrecyclerview/src/main/java/com/canyinghao/canrecyclerview/GridLayoutManagerFix.java
@@ -0,0 +1,33 @@
+package com.canyinghao.canrecyclerview;
+
+import android.content.Context;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+
+public class GridLayoutManagerFix extends GridLayoutManager {
+
+
+    public GridLayoutManagerFix(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public GridLayoutManagerFix(Context context, int spanCount) {
+        super(context, spanCount);
+    }
+
+    public GridLayoutManagerFix(Context context, int spanCount, int orientation, boolean reverseLayout) {
+        super(context, spanCount, orientation, reverseLayout);
+    }
+
+
+    @Override
+    public void collectAdjacentPrefetchPositions(int dx, int dy, RecyclerView.State state, LayoutPrefetchRegistry layoutPrefetchRegistry) {
+        try {
+            super.collectAdjacentPrefetchPositions(dx, dy, state, layoutPrefetchRegistry);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+}
